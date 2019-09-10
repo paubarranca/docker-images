97133a43f97e7deb54a846364d42c6bc83f9b6e0
paubarranca <paubarranca91@gmail.com>
2019-09-10 23:10:44 +0000

    Modified entrypoint.sh

--- haproxy/content/entrypoint.sh
+++ haproxy/content/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Create cfg file from template
-j2 /jinja2/haproxy.j2  /jinja2/haproxy.yaml > /etc/haproxy/haproxy.cfg
+/usr/bin/j2 /jinja2/haproxy.j2  /jinja2/haproxy.yaml > /etc/haproxy/haproxy.cfg
 
 exec /usr/sbin/haproxy -db -f /etc/haproxy/haproxy.cfg
