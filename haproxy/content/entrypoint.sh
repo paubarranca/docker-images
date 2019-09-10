553cc63153e2df8cee442cd9b8ae74e0fb4316ed
paubarranca <paubarranca91@gmail.com>
2019-09-10 18:11:36 +0000

    Implemented Jinja templates

--- haproxy/content/entrypoint.sh
+++ haproxy/content/entrypoint.sh
@@ -6,7 +6,7 @@ useradd --system --gid ${HAPROXY_GROUP} ${HAPROXY_USER}
 mkdir --parents /var/lib/${HAPROXY_USER}
 chown -R ${HAPROXY_USER}:${HAPROXY_GROUP} /var/lib/${HAPROXY_USER}
 
-# Create cfg file from YAML
-/usr/bin/j2 /jinja2/templates/haproxy.tpl  /jinja2/yaml/haproxy.yaml > /etc/haproxy/haproxy.cfg
+# Create cfg file from template
+/usr/bin/j2 /jinja2/haproxy.j2  /jinja2/haproxy.yaml > /etc/haproxy/haproxy.cfg
 
 exec /usr/sbin/haproxy -db -f /etc/haproxy/haproxy.cfg
