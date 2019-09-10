c3d42a5bc9d2ccd858ae1e706b89e073772b12c4
paubarranca <paubarranca91@gmail.com>
2019-09-10 17:59:18 +0000

    Testing jinja

--- haproxy/content/entrypoint.sh
+++ haproxy/content/entrypoint.sh
@@ -6,4 +6,7 @@ useradd --system --gid ${HAPROXY_GROUP} ${HAPROXY_USER}
 mkdir --parents /var/lib/${HAPROXY_USER}
 chown -R ${HAPROXY_USER}:${HAPROXY_GROUP} /var/lib/${HAPROXY_USER}
 
+# Create cfg file from YAML
+/usr/bin/j2 /jinja2/templates/haproxy.tpl  /jinja2/yaml/haproxy.yaml > /etc/haproxy/haproxy.cfg
+
 exec /usr/sbin/haproxy -db -f /etc/haproxy/haproxy.cfg
