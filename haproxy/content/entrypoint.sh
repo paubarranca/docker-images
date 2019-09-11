224c48ef4300984b768a32047dc2f3514bf214cb
paubarranca <paubarranca91@gmail.com>
2019-09-11 14:55:40 +0000

    Modified Jinja2 path

--- haproxy/content/entrypoint.sh
+++ haproxy/content/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Create cfg file from template
-/usr/bin/j2 /jinja2/haproxy.j2  /jinja2/yaml/haproxy.yaml > /etc/haproxy/haproxy.cfg
+/usr/bin/j2 /root/jinja2/haproxy.j2  /root/jinja2/yaml/haproxy.yaml > /etc/haproxy/haproxy.cfg
 
 exec /usr/sbin/haproxy -db -f /etc/haproxy/haproxy.cfg
