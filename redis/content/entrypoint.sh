3bd776ac69887d70be5afce61ea809455c044275
paubarranca <paubarranca91@gmail.com>
2019-09-12 19:24:40 +0000

    Implemented redis image

---
+++ redis/content/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+REDIS_PASSWORD=${REDIS_PASSWORD:-}
+
+map_redis_uid() {
+  USERMAP_ORIG_UID=$(id -u redis)
+  USERMAP_ORIG_GID=$(id -g redis)
+  USERMAP_GID=${USERMAP_GID:-${USERMAP_UID:-$USERMAP_ORIG_GID}}
+  USERMAP_UID=${USERMAP_UID:-$USERMAP_ORIG_UID}
+  if [ "${USERMAP_UID}" != "${USERMAP_ORIG_UID}" ] || [ "${USERMAP_GID}" != "${USERMAP_ORIG_GID}" ]; then
+    echo "Adapting uid and gid for redis:redis to $USERMAP_UID:$USERMAP_GID"
+    groupmod -g "${USERMAP_GID}" redis
+    sed -i -e "s/:${USERMAP_ORIG_UID}:${USERMAP_GID}:/:${USERMAP_UID}:${USERMAP_GID}:/" /etc/passwd
+  fi
+}
+
+map_redis_uid
+
+# allow arguments to be passed to redis-server
+if [[ ${1:0:1} = '-' ]]; then
+  EXTRA_ARGS="$@"
+  set --
+fi
+
+# default behaviour is to launch redis-server
+if [[ -z ${1} ]]; then
+  echo "Starting redis-server..."
+  exec start-stop-daemon --start --chuid ${REDIS_USER}:${REDIS_USER} --exec $(which redis-server) -- \
+    /etc/redis/redis.conf ${REDIS_PASSWORD:+--requirepass $REDIS_PASSWORD} ${EXTRA_ARGS}
+else
+  exec "$@"
+fi
