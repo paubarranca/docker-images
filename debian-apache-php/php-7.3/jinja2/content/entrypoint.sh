1cf3c7015d7afa67c4a5d57b669fe4f71b9b7bd6
paubarranca <paubarranca91@gmail.com>
2020-01-19 00:17:46 +0000

    Implemented jinja

--- debian-apache-php/php-7.3/jinja2/content/entrypoint.sh
+++ debian-apache-php/php-7.3/jinja2/content/entrypoint.sh
@@ -1,2 +1,19 @@
 #!/bin/bash -e
 
+# Apache config files
+j2 /root/jinja2/apache-conf.j2 > /etc/apache2/apache2.conf
+j2 /root/jinja2/apache-mpm.j2 > /etc/apache2/mods-enabled/mpm_event.conf
+j2 /root/jinja2/apache-site.j2 > /etc/apache2/sites-enabled/000-default.conf
+j2 /root/jinja2/apache-php-fpm-conf.j2 > /etc/apache2/conf-enabled/php${PHP_V}-fpm.conf
+
+# PHP config files
+j2 /root/jinja2/php-fpm-conf.j2 > /etc/php/${PHP_V}/fpm/php-fpm.conf
+j2 /root/jinja2/php-extras.j2 > /etc/php/${PHP_V}/apache2/conf.d/25-extras.ini
+
+if [[ "$#" < "1" ]]; then
+  exec supervisord -n -c /etc/supervisor/supervisord.conf
+else
+  exec $@
+fi
+
+apachectl -D FOREGROUND 
