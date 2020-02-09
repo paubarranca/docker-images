aa7e99e9bd8201bd14fe33eb9dd05da704ae5450
paubarranca <paubarranca91@gmail.com>
2020-02-09 19:44:03 +0000

    Changed nginx config

--- debian-nginx-php/php-7.3/content/entrypoint.sh
+++ debian-nginx-php/php-7.3/content/entrypoint.sh
@@ -7,7 +7,7 @@ j2 /root/jinja2/nginx-remoteip.j2 > /etc/nginx/conf.d/remoteip.conf
 
 # PHP config files
 j2 /root/jinja2/php-fpm-conf.j2 > /etc/php/${PHP_V}/fpm/php-fpm.conf
-j2 /root/jinja2/php-extras.j2 > /etc/php/${PHP_V}/nginx/conf.d/25-extras.ini
+# j2 /root/jinja2/php-extras.j2 > /etc/php/${PHP_V}/nginx/conf.d/25-extras.ini
 
 # Supervisor config file
 j2 /root/jinja2/supervisord.j2 > /etc/supervisor/supervisord.conf
